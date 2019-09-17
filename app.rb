5468ea88388720805c65545629983908d845d397
Lukas Gentele <lg@devspace.cloud>
2019-09-16 20:44:52 -0700

    improve hello world text

--- app.rb
+++ app.rb
@@ -19,7 +19,27 @@ loop do
   # Log the request to the console for debugging
   STDERR.puts request
 
-  response = "Hello World DevSpace!\n"
+  response = `
+  <html>
+      <head>
+          <link rel="stylesheet" href="https://devspace.cloud/docs/quickstart.css">
+      </head>
+      <body>
+          <img src="https://devspace.cloud/img/congrats.gif" />
+          <h1>You deployed this project with DevSpace!</h1>
+          <div>
+              <h2>Now it's time to start the development mode:</h2>
+              <ol>
+                  <li>Press CTRL+C or ENTER to terminate <code>devspace open</code></li>
+                  <li>Run: <code>devspace dev</code></li>
+                  <li>Edit this text in <code>index.js</code> and save the file</li>
+                  <li>Check the logs to see how <code>nodemon</code> recompiles and restarts this project</li>
+                  <li>Reload browser to see the changes</li>
+              </ol>
+          </div>
+      </body>
+  </html>
+  `
 
   # We need to include the Content-Type and Content-Length headers
   # to let the client know the size and type of data
