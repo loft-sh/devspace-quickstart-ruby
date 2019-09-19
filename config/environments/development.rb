01ae5d930a870c771705e87a7b10a7dcfbce753e
Lukas Gentele <lg@devspace.cloud>
2019-09-19 05:26:19 -0700

    allow all hostnames during development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Make sure we can use any hostname in development
+  config.hosts.clear
 end
